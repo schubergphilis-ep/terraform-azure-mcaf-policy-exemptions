d6e32ab028cceec15031034105cdd719abbdd31e
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-05-28 12:59:32 +0200

    fix pinned versions

--- terraform.tf
+++ terraform.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4"
+      version = "~> 4.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "2.0.1"
+      version = "~> 2.0"
     }
   }
 }
